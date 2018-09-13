7c6e1793cd97dfc4eccb3a8b6f9bdd99163f54ad
Svetlozar Todorov <svetlik@gmail.com>
2018-09-13 22:57:31 +0300

    Add some text, conditionally, and by list

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <!-- <p>{{ message }}</p> -->
+    <!-- <p v-bind:title="message">
+      Hover your mouse over me for a few seconds
+      to see my dynamically bound title!
+    </p> -->
+    <!-- <div id="app-3">
+      <span v-if="seen">Now you see me</span>
+    </div> -->
+    <div id="app-4">
+      <ol>
+        <li v-bind:key="todo" v-for="todo in todos">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
@@ -8,7 +22,13 @@
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      // message: 'You loaded this page on ' + new Date().toLocaleString(),
+      // seen: true,
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
     }
   }
 }
