2e891d718af8546a07655b69068be9e365fe029b
Svetlozar Todorov <svetlik@gmail.com>
2018-09-20 10:23:58 +0300

    Experiments in single file component extraction

---
+++ app/javascript/components/TweetColumn.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tweet-column" :class="$mq">
+    <h3>{{title}}</h3>
+    <draggable v-model=tweets @start="drag=true" @end="drag=false">
+      <p v-bind:key="tweet.id" v-for="tweet in tweets">
+        {{ tweet.text }}
+      </p>
+    </draggable>
+
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+  name: 'TweetColumn',
+  components: {
+    draggable
+  },
+  props: ["title", "tweets"],
+  data: function() {
+    return {
+    }
+  },
+}
+</script>
+
+<style scoped>
+  h3 { text-align: center; }
+
+  p {
+    padding: 0 10px;
+    margin: 5px;
+    border: solid #30B0E9 2px;
+    border-radius: 10px;
+  }
+
+  .tweet-column {
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .mobile { width: 90%; }
+
+  .tablet { width: 60%; }
+
+  .laptop { width: 40%; }
+
+  .desktop { width: 30%; }
+
+</style>
--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -17,33 +17,9 @@
     </div>
 
     <div class="container">
-        <div id="xbav-tweets" class="tweet-column" :class="$mq">
-          <h3>@xbav_ag</h3>
-          <draggable v-model="tweetsXbav" @start="drag=true" @end="drag=false">
-            <p v-bind:key="tweet.id" v-for="tweet in tweetsXbav">
-              {{ tweet.text }}
-            </p>
-          </draggable>
-
-        </div>
-
-      <div id="github-tweets" class="tweet-column" :class="$mq">
-        <h3>@github</h3>
-        <draggable v-model="tweetsGithub" @start="drag=true" @end="drag=false">
-          <p v-bind:key="tweet.id" v-for="tweet in tweetsGithub">
-            {{ tweet.text }}
-          </p>
-        </draggable>
-      </div>
-
-      <div id="github-tweets" class="tweet-column" :class="$mq">
-        <h3>@vuejs</h3>
-        <draggable v-model="tweetsVuejs" @start="drag=true" @end="drag=false">
-          <p v-bind:key="tweet.id" v-for="tweet in tweetsVuejs">
-            {{ tweet.text }}
-          </p>
-        </draggable>
-      </div>
+      <TweetColumn :tweets=tweetsXbav title="@xbav_ag"></TweetColumn>
+      <TweetColumn :tweets=tweetsGithub title="@github"></TweetColumn>
+      <TweetColumn :tweets=tweetsVuejs title="@vuejs"></TweetColumn>
     </div>
   </div>
 </template>
@@ -51,10 +27,12 @@
 <script>
   import draggable from 'vuedraggable'
   import TweetStore from '../store/TweetStore'
+  import TweetColumn from "./TweetColumn";
 
   export default {
     components: {
       draggable,
+      TweetColumn
     },
     props: ["tweetsXbav","tweetsGithub","tweetsVuejs","showLess"],
     data: function () {
@@ -93,15 +71,6 @@
 
   input[type=text]:focus { width: 50%; }
 
-  h3 { text-align: center; }
-
-  p {
-    padding: 0 10px;
-    margin: 5px;
-    border: solid #30B0E9 2px;
-    border-radius: 10px;
-  }
-
   li {
     padding: 10px;
     border: solid white 2px;
@@ -117,28 +86,6 @@
     margin: 0;
     padding-top: 30px;
     text-align: center;
-  }
-
-  .tweet-column {
-    display: flex;
-    flex-direction: column;
-    margin-top: 40px;
-    margin-bottom: 40px;
-    margin-left: 10px;
-    margin-right: 10px;
-    width: 30%;
-  }
-
-  .mobile { width: 100%; }
-
-  .tablet { width: 60%; }
-
-  .laptop { width: 40%; }
-
-  .desktop { width: 30%; }
-
-  .light-blue {
-    color: #30B0E9;
   }
 
 </style>
