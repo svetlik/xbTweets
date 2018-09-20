932130018163680856cea04edbc36884c9c57078
Svetlozar Todorov <svetlik@gmail.com>
2018-09-20 11:11:59 +0300

    Implement tweet column persistence with LocalStorage
    Successfully implemented data persistence with LocalStorage.
    
    TODO: Figure out how to keep using the component and store the data uniquely for each column.

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -17,9 +17,38 @@
     </div>
 
     <div class="container">
-      <TweetColumn :tweets=tweetsXbav title="@xbav_ag"></TweetColumn>
-      <TweetColumn :tweets=tweetsGithub title="@github"></TweetColumn>
-      <TweetColumn :tweets=tweetsVuejs title="@vuejs"></TweetColumn>
+      <!-- <TweetColumn :tweets=tweetsXbav title="@xbav_ag"></TweetColumn> -->
+      <!-- <TweetColumn :tweets=tweetsGithub title="@github"></TweetColumn> -->
+      <!-- <TweetColumn :tweets=tweetsVuejs title="@vuejs"></TweetColumn> -->
+      <div class="tweet-column" :class="$mq">
+        <h3>@xbav_ag</h3>
+        <draggable v-model=tweetsXbav @start="drag=true" @end="drag=false">
+          <p v-bind:key="tweet.id" v-for="tweet in tweetsXbav">
+            {{ tweet.text }}
+          </p>
+        </draggable>
+
+      </div>
+
+      <div class="tweet-column" :class="$mq">
+        <h3>@github</h3>
+        <draggable v-model=tweetsGithub @start="drag=true" @end="drag=false">
+          <p v-bind:key="tweet.id" v-for="tweet in tweetsGithub">
+            {{ tweet.text }}
+          </p>
+        </draggable>
+
+      </div>
+
+      <div class="tweet-column" :class="$mq">
+        <h3>@vuejs</h3>
+        <draggable v-model=tweetsVuejs @start="drag=true" @end="drag=false">
+          <p v-bind:key="tweet.id" v-for="tweet in tweetsVuejs">
+            {{ tweet.text }}
+          </p>
+        </draggable>
+
+      </div>
     </div>
   </div>
 </template>
@@ -44,6 +73,39 @@
       increment () {
         TweetStore.methods.increment()
       }
+    },
+    mounted() {
+      console.log('App mounted!');
+      console.log(localStorage)
+      if (localStorage.getItem('tweetsXbav')) this.tweetsXbav = JSON.parse(localStorage.getItem('tweetsXbav'));
+      if (localStorage.getItem('tweetsGithub')) this.tweetsGithub = JSON.parse(localStorage.getItem('tweetsGithub'));
+      if (localStorage.getItem('tweetsVuejs')) this.tweetsVuejs = JSON.parse(localStorage.getItem('tweetsVuejs'));
+    },
+    watch: {
+      tweetsXbav: {
+        handler() {
+          console.log('TweetsXbav changed!');
+          localStorage.setItem('tweetsXbav', JSON.stringify(this.tweetsXbav));
+        },
+
+        deep: true,
+      },
+      tweetsGithub: {
+        handler() {
+          console.log('TweetsGithub changed!');
+          localStorage.setItem('tweetsGithub', JSON.stringify(this.tweetsGithub));
+        },
+
+        deep: true,
+      },
+      tweetsVuejs: {
+        handler() {
+          console.log('TweetsVuejs changed!');
+          localStorage.setItem('tweetsVuejs', JSON.stringify(this.tweetsVuejs));
+        },
+
+        deep: true,
+      },
     },
   }
 </script>
@@ -87,6 +149,32 @@
     padding-top: 30px;
     text-align: center;
   }
+
+  h3 { text-align: center; }
+
+  p {
+    padding: 0 10px;
+    margin: 5px;
+    border: solid #30B0E9 2px;
+    border-radius: 10px;
+  }
+
+  .tweet-column {
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .mobile { width: 90%; }
+
+  .tablet { width: 60%; }
+
+  .laptop { width: 40%; }
+
+  .desktop { width: 30%; }
 
 
 </style>
